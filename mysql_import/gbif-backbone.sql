8804a7b01b626ddbc838fecc49e3a565ff0eb62d
Boey <khcy5bjw@nottingham.edu.my>
2017-01-11 20:46:54 +0800

    GBIF and NCBI import

---
+++ mysql_import/gbif-backbone.sql
@@ -0,0 +1,138 @@
+SET autocommit = 0;
+SET unique_checks = 0;
+SET foreign_key_checks = 0;
+
+#Drop any table with same names in foreign key order
+DROP TABLE IF EXISTS `gbif_reference`;
+DROP TABLE IF EXISTS `gbif_multimedia`;
+DROP TABLE IF EXISTS `gbif_vernacularname`;
+DROP TABLE IF EXISTS `gbif_distribution`;
+DROP TABLE IF EXISTS `gbif_taxon`;
+
+#taxon.txt
+CREATE TABLE `gbif_taxon` (
+	`coreID` int unsigned NOT NULL default '0',
+	`taxonID` int unsigned NOT NULL default '0',
+	`datasetID` char(36) NOT NULL default '',
+	`parentNameUsageID` int unsigned default NULL,
+	`acceptedNameUsageID` int unsigned default NULL,
+	`scientificName` varchar(255) NOT NULL default '',
+	`taxonRank` varchar(255) NOT NULL default '',
+	`nameAccordingTo` varchar(255) default NULL,
+	`namePublishedIn` varchar(255) default NULL,
+	`taxonomicStatus` varchar(255) default NULL,
+	`nomenclaturalStatus` varchar(255) default NULL,
+	`kingdom` varchar(255) default NULL,
+	`phylum` varchar(255) default NULL,
+	`class` varchar(255) default NULL,
+	`order` varchar(255) default NULL,
+	`family` varchar(255) default NULL,
+	`genus` varchar(255) default NULL,
+	`taxonRemarks` varchar(255) default NULL
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/taxon.txt'
+	INTO TABLE `gbif_taxon`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t'
+	LINES TERMINATED BY '\n';
+COMMIT;
+
+ALTER TABLE `gbif_taxon` ADD CONSTRAINT `pk-coreID` PRIMARY KEY (`coreID`);
+COMMIT;
+
+#references.txt
+CREATE TABLE `gbif_reference` (
+	`coreID` int unsigned NOT NULL default '0',
+	`bibliographicCitation` varchar(255) NOT NULL default '',
+	`references` varchar(255) default NULL,
+	`source` varchar(255) default NULL,
+	`identifier` varchar(255) default NULL
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/reference.txt'
+	INTO TABLE `gbif_reference`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t'
+	LINES TERMINATED BY '\n';
+COMMIT;
+
+ALTER TABLE `gbif_reference` ADD CONSTRAINT `fk-coreID-reference` FOREIGN KEY (`coreID`) REFERENCES `gbif_taxon`(`coreID`);
+COMMIT;
+	
+#multimedia.txt
+CREATE TABLE `gbif_multimedia` (
+	`coreID` int unsigned NOT NULL default '0',
+	`references` varchar(255) NOT NULL default '',
+	`description` varchar(255) default NULL,
+	`title` varchar(255) default NULL,
+	`contributor` varchar(255) default NULL,
+	`source` varchar(255) default NULL,
+	`created` varchar(255) default NULL,
+	`license` varchar(255) default NULL,
+	`identifier` varchar(255) default NULL,
+	`creator` varchar(255) default NULL,
+	`publisher` varchar(255) default NULL,
+	`rightsHolder` varchar(255) default NULL
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/multimedia.txt'
+	INTO TABLE `gbif_multimedia`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t'
+	LINES TERMINATED BY '\n';
+COMMIT;
+
+ALTER TABLE `gbif_multimedia` ADD CONSTRAINT `fk-coreID-multimedia` FOREIGN KEY (`coreID`) REFERENCES `gbif_taxon`(`coreID`);
+COMMIT;
+	
+#vernacularname.txt
+CREATE TABLE `gbif_vernacularname` (
+	`coreID` int unsigned NOT NULL default '0',
+	`vernacularName` varchar(255) NOT NULL default '',
+	`source` varchar(255) default NULL,
+	`sex` varchar(255) default NULL,
+	`lifeStage` varchar(255) default NULL,
+	`language` varchar(255) default NULL,
+	`countryCode` varchar(255) default NULL,
+	`country` varchar(255) default NULL
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/vernacularname.txt'
+	INTO TABLE `gbif_vernacularname`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t'
+	LINES TERMINATED BY '\n';
+COMMIT;
+
+ALTER TABLE `gbif_vernacularname` ADD CONSTRAINT `fk-coreID-vernacularname` FOREIGN KEY (`coreID`) REFERENCES `gbif_taxon`(`coreID`);
+COMMIT;
+	
+#distribution.txt
+CREATE TABLE `gbif_distribution` (
+	`coreID` int unsigned NOT NULL default '0',
+	`source` varchar(255) NOT NULL default '',
+	`threatStatus` varchar(255) default NULL,
+	`locality` varchar(255) default NULL,
+	`lifeStage` varchar(255) default NULL,
+	`occuranceStatus` varchar(255) default NULL,
+	`locationID` varchar(255) default NULL,
+	`locationRemarks` varchar(255) default NULL,
+	`establishmentMeans` varchar(255) default NULL,
+	`countryCode` varchar(255) default NULL,
+	`country` varchar(255) default NULL
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/distribution.txt'
+	INTO TABLE `gbif_distribution`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t'
+	LINES TERMINATED BY '\n';
+COMMIT;
+
+ALTER TABLE `gbif_distribution` ADD CONSTRAINT `fk-coreID-distribution` FOREIGN KEY (`coreID`) REFERENCES `gbif_taxon`(`coreID`);
+COMMIT;
+
+SET autocommit = 1;
+SET unique_checks = 1;
+SET foreign_key_checks = 1;
---
+++ mysql_import/taxdump.sql
@@ -0,0 +1,152 @@
+SET autocommit = 0;
+SET unique_checks = 0;
+SET foreign_key_checks = 0;
+
+#Drop any table with same names in foreign key order
+DROP TABLE IF EXISTS `ncbi_delnodes`;
+DROP TABLE IF EXISTS `ncbi_merged`;
+DROP TABLE IF EXISTS `ncbi_citations`;
+DROP TABLE IF EXISTS `ncbi_names`;
+DROP TABLE IF EXISTS `ncbi_nodes`;
+DROP TABLE IF EXISTS `ncbi_division`;
+DROP TABLE IF EXISTS `ncbi_gencode`;
+
+#division.dmp
+CREATE TABLE `ncbi_division` (
+	`division_id` smallint(6) unsigned NOT NULL default '0',
+	`division_cde` char(3) NOT NULL default '',
+	`division_name` varchar(255) NOT NULL default '',
+	`comments` varchar(255) default NULL
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/division.dmp'
+	INTO TABLE `ncbi_division`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t|\t'
+	LINES TERMINATED BY '\t|\n';
+COMMIT;
+
+ALTER TABLE `ncbi_division` ADD CONSTRAINT `pk-division_id` PRIMARY KEY(`division_id`);
+COMMIT;
+	
+#gencode.dmp
+CREATE TABLE `ncbi_gencode` (
+	`genetic_code_id` smallint(6) unsigned NOT NULL default '0',
+	`abbreviation` varchar(255) default NULL,
+	`name` varchar(255) NOT NULL default '',
+	`cde` varchar(255) NOT NULL default '',
+	`starts` varchar(255) NOT NULL default ''
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/gencode.dmp'
+	INTO TABLE `ncbi_gencode`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t|\t'
+	LINES TERMINATED BY '\t|\n';
+COMMIT;
+
+ALTER TABLE `ncbi_gencode` ADD CONSTRAINT `pk-genetic_code_id` PRIMARY KEY (`genetic_code_id`);
+COMMIT;
+	
+#delnodes.dmp
+CREATE TABLE `ncbi_delnodes` (
+	`tax_id` mediumint(11) unsigned NOT NULL default '0'
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/delnodes.dmp'
+	INTO TABLE `ncbi_delnodes`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t|\t'
+	LINES TERMINATED BY '\t|\n';
+COMMIT;
+
+ALTER TABLE `ncbi_delnodes` ADD CONSTRAINT `uq-tax_id` UNIQUE (`tax_id`);
+COMMIT;
+	
+#merged.dmp
+CREATE TABLE `ncbi_merged` (
+	`old_tax_id` mediumint(11) unsigned NOT NULL default '0',
+	`new_tax_id` mediumint(11) unsigned NOT NULL default '0'
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/merged.dmp'
+	INTO TABLE `ncbi_merged`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t|\t'
+	LINES TERMINATED BY '\t|\n';
+COMMIT;
+
+ALTER TABLE `ncbi_merged` ADD CONSTRAINT `uq-old_tax_id` UNIQUE (`old_tax_id`);
+COMMIT;
+	
+#citations.dmp
+CREATE TABLE `ncbi_citations` (
+	`cit_id` mediumint(11) unsigned NOT NULL default '0',
+	`cit_key` varchar(255) NOT NULL default '',
+	`pubmed_id` mediumint(11) unsigned NOT NULL default '0',
+	`medline_id` mediumint(11) unsigned NOT NULL default '0',
+	`url` varchar(255) NOT NULL default '',
+	`text` varchar(255) default NULL,
+	`taxid_list` varchar(255) NOT NULL default '0'
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/citations.dmp'
+	INTO TABLE `ncbi_citations`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t|\t'
+	LINES TERMINATED BY '\t|\n';
+COMMIT;
+
+ALTER TABLE `ncbi_citations` ADD CONSTRAINT `pk-cit_id` PRIMARY KEY (`cit_id`);
+COMMIT;
+
+#nodes.dmp
+CREATE TABLE `ncbi_nodes` (
+  `tax_id` mediumint(11) unsigned NOT NULL default '0',
+  `parent_tax_id` mediumint(8) unsigned NOT NULL default '0',
+  `rank` varchar(32) default NULL,
+  `embl_code` varchar(16) default NULL,
+  `division_id` smallint(6) unsigned NOT NULL default '0',
+  `inherited_div_flag` tinyint(4) NOT NULL default '0',
+  `genetic_code_id` smallint(6) unsigned NOT NULL default '0',
+  `inherited_GC_flag` tinyint(4) NOT NULL default '0',
+  `mitochondrial_genetic_code_id` smallint(4) NOT NULL default '0',
+  `inherited_MGC_flag` tinyint(4) NOT NULL default '0',
+  `GenBank_hidden_flag` smallint(4) NOT NULL default '0',
+  `hidden_subtree_root_flag` tinyint(4) NOT NULL default '0',
+  `comments` varchar(255) default NULL
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/nodes.dmp'
+	INTO TABLE `ncbi_nodes`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t|\t'
+	LINES TERMINATED BY '\t|\n';
+COMMIT;
+
+ALTER TABLE `ncbi_nodes`
+	ADD CONSTRAINT `pk-tax_id` PRIMARY KEY (`tax_id`),
+	ADD CONSTRAINT `fk-division_id` FOREIGN KEY (`division_id`) REFERENCES `ncbi_division` (`division_id`),
+	ADD CONSTRAINT `fk-gencode_code_id` FOREIGN KEY (`genetic_code_id`) REFERENCES `ncbi_gencode` (`genetic_code_id`);
+COMMIT;
+
+#names.dmp
+CREATE TABLE `ncbi_names` (
+  `tax_id` mediumint(11) unsigned NOT NULL default '0',
+  `name_txt` varchar(255) NOT NULL default '',
+  `unique_name` varchar(255) default NULL,
+  `name_class` varchar(32) NOT NULL default ''
+);
+
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/names.dmp'
+	INTO TABLE `ncbi_names`
+	CHARACTER SET utf8mb4
+	FIELDS TERMINATED BY '\t|\t'
+	LINES TERMINATED BY '\t|\n';
+COMMIT;
+
+ALTER TABLE `ncbi_names` ADD CONSTRAINT `fk-tax_id` FOREIGN KEY (`tax_id`) REFERENCES `ncbi_nodes`(`tax_id`);
+
+SET autocommit = 1;
+SET unique_checks = 1;
+SET foreign_key_checks = 1;
