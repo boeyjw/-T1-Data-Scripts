4fea08212ec7c1a5b64b4ffebf72335d8c2013ac
Boey <khcy5bjw@nottingham.edu.my>
2017-01-22 15:52:58 +0800

    Update multi error. Something wrong in referencing

--- mongo_import/gbif_tr.rb
+++ mongo_import/gbif_tr.rb
@@ -1,41 +1,4 @@
-table "gbif_distribution", :rename_to => 'distribution', :embed_in => 'taxon', :on => 'taxon' do
-	column "coreID", :integer, :references => :taxon
-	column "source", :string
-	column "threatStatus", :string
-	column "locality", :string
-	column "lifeStage", :string
-	column "occuranceStatus", :string
-	column "locationID", :string
-	column "locationRemarks", :string
-	column "establishmentMeans", :string
-	column "countryCode", :string
-	column "country", :string
-end
-
-table "gbif_multimedia", :rename_to => 'multimedia', :embed_in => 'taxon', :on => 'taxon' do
-	column "coreID", :integer, :references => :taxon
-	column "references", :string
-	column "description", :string
-	column "title", :string
-	column "contributor", :string
-	column "source", :string
-	column "created", :string
-	column "license", :string
-	column "identifier", :string
-	column "creator", :string
-	column "publisher", :string
-	column "rightsHolder", :string
-end
-
-table "gbif_reference", :rename_to => 'reference', :embed_in => 'taxon', :on => 'taxon' do
-	column "coreID", :integer, :references => :taxon
-	column "bibliographicCitation", :string
-	column "references", :string
-	column "source", :string
-	column "identifier", :string
-end
-
-table "gbif_taxon", :rename_to => ''taxon'' do
+table "gbif_taxon", :rename_to => 'taxon' do
 	column "coreID", :key
 	column "taxonID", :integer
 	column "datasetID", :string
@@ -56,7 +19,44 @@ table "gbif_taxon", :rename_to => ''taxon'' do
 	column "taxonRemarks", :string
 end
 
-table "gbif_vernacularname", :rename_to => 'vernacularname', :embed_in => 'taxon', :on => 'taxon' do
+table "gbif_distribution", :rename_to => 'distribution', :embed_in => 'taxon', :on => 'coreID' do
+	column "coreID", :integer, :references => :taxon
+	column "source", :string
+	column "threatStatus", :string
+	column "locality", :string
+	column "lifeStage", :string
+	column "occuranceStatus", :string
+	column "locationID", :string
+	column "locationRemarks", :string
+	column "establishmentMeans", :string
+	column "countryCode", :string
+	column "country", :string
+end
+
+table "gbif_multimedia", :rename_to => 'multimedia', :embed_in => 'taxon', :on => 'coreID' do
+	column "coreID", :integer, :references => :taxon
+	column "references", :string
+	column "description", :string
+	column "title", :string
+	column "contributor", :string
+	column "source", :string
+	column "created", :string
+	column "license", :string
+	column "identifier", :string
+	column "creator", :string
+	column "publisher", :string
+	column "rightsHolder", :string
+end
+
+table "gbif_reference", :rename_to => 'reference', :embed_in => 'taxon', :on => 'coreID' do
+	column "coreID", :integer, :references => :taxon
+	column "bibliographicCitation", :string
+	column "references", :string
+	column "source", :string
+	column "identifier", :string
+end
+
+table "gbif_vernacularname", :rename_to => 'vernacularname', :embed_in => 'taxon', :on => 'coreID' do
 	column "coreID", :integer, :references => :taxon
 	column "vernacularName", :string
 	column "source", :string
